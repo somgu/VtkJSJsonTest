441f8b6de60150c7fb4d588b2cd4edfe5493a4e6
Dave.oh <sangseok@healthhub.kr>
2022-01-04 10:33:47 +0900

    project init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import '@kitware/vtk.js/Rendering/Profiles/*';
+import "@kitware/vtk.js/Rendering/Profiles/All";
 
-import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkFullScreenRenderWindow from "@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow";
 
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkCalculator      from '@kitware/vtk.js/Filters/General/Calculator';
-import vtkConeSource      from '@kitware/vtk.js/Filters/Sources/ConeSource';
-import vtkJSONReader      from '@kitware/vtk.js/IO/Misc/JSONReader';
-import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
+import vtkCalculator from "@kitware/vtk.js/Filters/General/Calculator";
+import vtkConeSource from "@kitware/vtk.js/Filters/Sources/ConeSource";
+import vtkJSONReader from "@kitware/vtk.js/IO/Misc/JSONReader";
+import vtkHttpDataSetReader from "@kitware/vtk.js/IO/Core/HttpDataSetReader";
+import "@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper";
 
-import { AttributeTypes } from '@kitware/vtk.js/Common/DataModel/DataSetAttributes/Constants';
-import { FieldDataTypes } from '@kitware/vtk.js/Common/DataModel/DataSet/Constants';
+import { AttributeTypes } from "@kitware/vtk.js/Common/DataModel/DataSetAttributes/Constants";
+import { FieldDataTypes } from "@kitware/vtk.js/Common/DataModel/DataSet/Constants";
 
 const controlPanel = `
 <table>
@@ -50,14 +50,19 @@ const filter = vtkCalculator.newInstance();
 
 filter.setInputConnection(coneSource.getOutputPort());
 filter.setFormula({
-  getArrays: inputDataSets => ({
+  getArrays: (inputDataSets) => ({
     input: [],
     output: [
-      { location: FieldDataTypes.CELL, name: 'Random', dataType: 'Float32Array', attribute: AttributeTypes.SCALARS },
+      {
+        location: FieldDataTypes.CELL,
+        name: "Random",
+        dataType: "Float32Array",
+        attribute: AttributeTypes.SCALARS,
+      },
     ],
   }),
   evaluate: (arraysIn, arraysOut) => {
-    const [scalars] = arraysOut.map(d => d.getData());
+    const [scalars] = arraysOut.map((d) => d.getData());
     for (let i = 0; i < scalars.length; i++) {
       scalars[i] = Math.random();
     }
@@ -79,17 +84,28 @@ renderWindow.render();
 // -----------------------------------------------------------
 
 fullScreenRenderer.addController(controlPanel);
-const representationSelector = document.querySelector('.representations');
-const resolutionChange = document.querySelector('.resolution');
+const representationSelector = document.querySelector(".representations");
+const resolutionChange = document.querySelector(".resolution");
 
-representationSelector.addEventListener('change', (e) => {
+representationSelector.addEventListener("change", (e) => {
   const newRepValue = Number(e.target.value);
   actor.getProperty().setRepresentation(newRepValue);
   renderWindow.render();
 });
 
-resolutionChange.addEventListener('input', (e) => {
+resolutionChange.addEventListener("input", (e) => {
   const resolution = Number(e.target.value);
   coneSource.setResolution(resolution);
   renderWindow.render();
 });
+
+const API_URL = __API_URL__;
+fetch(`${API_URL}/json/test.json`)
+  .then((response) => {
+    console.log(response.status);
+    return response.json();
+  })
+  .then((json) => {
+    console.log("json", json, json.name);
+  })
+  .catch((err) => console.error(err));
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const API_URL = "http://localhost:8088";
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+
+const assets = [
+  {
+    from: path.resolve(__dirname, "public/json"),
+    to: path.resolve(OUTPUT_DIR, "json"),
+  },
+];
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: OUTPUT_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.worker\.js$/,
+        include: [/worker/, /vtk\.js[/\\]Sources/],
+        use: [
+          {
+            loader: "worker-loader",
+          },
+        ],
+      },
+      { test: /\.glsl$/i, loader: "shader-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+        options: {
+          name: "assets/fonts/[name].[ext]",
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "assets/images/[contenthash].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({}),
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(API_URL),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [...assets],
+    }),
+    new HtmlWebpackPlugin({
+      title: "VtkJS JSON Test",
+      inject: "body",
+      template: path.resolve(__dirname, "public/index.html"),
+      meta: {
+        "set-charset": {
+          charset: "utf-8",
+        },
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        description: "Healthhub VtkJS",
+        keywords: "Healthhub, vtkjs",
+        author: "Healthhub.co.LTD",
+        "X-UA-Compatible": {
+          "http-equiv": "X-UA-Compatible",
+          content: "IE=edge",
+        },
+        "set-content-type": {
+          "http-equive": "Content-Type",
+          content: "text/html",
+          charset: "utf-8",
+        },
+      },
+      chunks: ["main"],
+      filename: path.join(OUTPUT_DIR, "index.html"),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 8088,
+    liveReload: true,
+  },
+};
